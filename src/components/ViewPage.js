8106a05768650235d91a216cc7814fc940c35eda
mchocilowicz <marcin.chocilowicz@gmail.com>
2017-05-30 18:17:04 +0200

    Send request with axios to get pokemon info

--- src/components/ViewPage.js
+++ src/components/ViewPage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios';
 
-const ViewPage = ({ match }) => (
-  <div>
-    <h3>{match.params.pokemonId}</h3>
-  </div>
-)
+class ViewPage extends Component {
+  componentDidMount() {
+    axios
+      .get(`http://pokeapi.co/api/v2/pokemon/${this.props.match.params.pokemonId}`)
+      .then(res => res.data).then(console.log);
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>{this.props.match.params.pokemonId}</h3>
+      </div>
+    )
+  }
+}
 
 export default ViewPage
