66cc7b7ee1d0dd9b98e02984ffc5fa8c3d185766
mchocilowicz <marcin.chocilowicz@gmail.com>
2017-05-30 19:45:36 +0200

    Short About page text

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Header, Image } from 'semantic-ui-react'
 
 export default () => (
-  <h1>About</h1>
+  <Header as='h2' icon textAlign='center'>
+    <Image src='https://vignette3.wikia.nocookie.net/youtubepoop/images/4/4c/Pokeball.png/revision/latest?cb=20150418234807' />
+    <Header.Content>
+      {'Site created with React and Semantic UI. \n '}
+    </Header.Content>
+  </Header>
 )
