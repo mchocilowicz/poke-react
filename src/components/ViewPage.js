55b0d00d65e5bb2917e3a987c8d5ac0915529f83
mchocilowicz <marcin.chocilowicz@gmail.com>
2017-05-30 18:27:18 +0200

    Loading indicator when state is null

--- src/components/ViewPage.js
+++ src/components/ViewPage.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import { Loader } from 'semantic-ui-react'
 
 class ViewPage extends Component {
   componentDidMount() {
     axios
       .get(`http://pokeapi.co/api/v2/pokemon/${this.props.match.params.pokemonId}`)
-      .then(res => res.data).then(console.log);
+      .then(res => res.data).then(data => this.setState({ ...data }))
   }
 
   render () {
+    console.log(this.state)
+    if (!this.state) {
+      return <Loader active inline='centered' />
+    }
     return (
       <div>
         <h3>{this.props.match.params.pokemonId}</h3>
